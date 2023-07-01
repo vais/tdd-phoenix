7de121b59777a2539470cb690f14dbdb83e77ae6
Vais Salikhov <vsalikhov@gmail.com>
2023-07-01 15:17:00 -0400

    Clean up

--- lib/chatter_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule ChatterWeb.PageController do
-  use ChatterWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
